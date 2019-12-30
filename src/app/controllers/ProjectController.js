a9bbf6c2a54724b4d17ef33842456df0bcd1cb6d
Lune Bakami <lune.shinigami@gmail.com>
2019-12-30 16:41:53 -0300

    route get, put, delete /projects created

--- src/app/controllers/ProjectController.js
+++ src/app/controllers/ProjectController.js
@@ -26,6 +26,50 @@ class ProjectController {
       title,
     });
   }
+
+  async index(req, res) {
+    const projects = await Project.findAll({});
+
+    return res.json(projects);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails!' });
+    }
+
+    const { title } = req.body;
+
+    const project = await Project.findByPk(req.params.id);
+
+    if (title !== project.title) {
+      const projectExists = await Project.findOne({ where: { title } });
+
+      if (projectExists) {
+        return res.status(400).json({ error: 'Project already exists.' });
+      }
+    }
+
+    const projectUpdated = await project.update(req.body);
+
+    return res.json(projectUpdated);
+  }
+
+  async delete(req, res) {
+    const project = await Project.findByPk(req.params.id);
+
+    if (!project) {
+      return res.status(400).json({ error: "This project doesn't exists!" });
+    }
+
+    await project.destroy();
+
+    return res.json({ ok: true });
+  }
 }
 
 export default new ProjectController();
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,8 @@ import ProjectController from './app/controllers/ProjectController';
 const routes = new Router();
 
 routes.post('/projects', ProjectController.store);
+routes.get('/projects', ProjectController.index);
+routes.put('/projects/:id', ProjectController.update);
+routes.delete('/projects/:id', ProjectController.delete);
 
 export default routes;
