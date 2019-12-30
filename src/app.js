f2e47db183170788514a6f1cb0bb9a8c36ee5673
Lune Bakami <lune.shinigami@gmail.com>
2019-12-30 07:44:34 -0300

    Revert "eslint, sequelize and others configured"
    This reverts commit fcd4fe13f501d598489086380bf972852f9f32a7.

--- src/app.js
+++ src/app.js
@@ -1,19 +1,8 @@
 import express from 'express';
-import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
-    this.middlewares();
-    this.routes();
-  }
-
-  middlewares() {
-    this.server.use(express.json());
-  }
-
-  routes() {
-    this.server.use(routes);
   }
 }
 
--- src/routes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-
-const routes = new Router();
-
-export default routes;
